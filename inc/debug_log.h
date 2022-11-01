7c4ac57cbc7ba6653c0ebb319b75f35798591304
pnt325 <phat.nt@hotmail.com>
2022-11-01 15:19:15 +0700

    Update assert condition

--- inc/debug_log.h
+++ inc/debug_log.h
@@ -1,16 +1,18 @@
 #ifndef _DEBUG_LOG_H_
 #define _DEBUG_LOG_H_
 
+#include <stdint.h>
+
 /* Macro */
 #define USE_DEBUG_LOG
-#define USE_DEBUG_ASSERT
+//#define USE_DEBUG_ASSERT
 #define USE_DEBUG_ASSERT_FULL
 
 
 #ifdef USE_DEBUG_LOG
 
 /* TODO Comment this macro if don't use timestampe in log */
-#define USE_DEBUG_LOG_TIMESTAMP
+//#define USE_DEBUG_LOG_TIMESTAMP
 
 #include "../utils/SEGGER_RTT/RTT/SEGGER_RTT.h"
 #define debug_log_init()                SEGGER_RTT_ConfigUpBuffer(0, NULL, NULL, 0, SEGGER_RTT_MODE_NO_BLOCK_SKIP);	\
@@ -39,7 +41,6 @@ uint32_t __debug_log_gettick_ms(void);
 #if defined(USE_DEBUG_ASSERT) && defined(USE_DEBUG_ASSERT_FULL)
 #error "! Multi assert defined !"
 #else
-
 #if defined(USE_DEBUG_ASSERT)
 void __debug_assert(void);
 #define debug_assert(__v)		(__v)?(void)0: __debug_assert()
@@ -48,8 +49,9 @@ void __debug_assert(const char* _f, int _l);
 #define debug_assert(__v)							\
 	do												\
 	{												\
-		if(__v)										\
-			__debug_assert(__FUNC__, __LINE__)		\
+		if(__v);									\
+		else										\
+			__debug_assert(__func__, __LINE__);		\
 	} while (0);
 #else
 #define debug_assert(__v)
